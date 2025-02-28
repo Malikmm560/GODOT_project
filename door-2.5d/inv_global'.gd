52dc66b65a4f53773bd938b08dcd010a51ffbbca
thorfinnking1 <157901908+thorfinnking1@users.noreply.github.com>
2025-02-28 21:07:14 +0200

    inv

---
+++ door-2.5d/inv_global'.gd
@@ -0,0 +1,13 @@
+extends Node
+
+var i = 2
+var table = {"1":i}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	print(table["1"])
