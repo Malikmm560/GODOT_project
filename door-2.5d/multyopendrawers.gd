44bbad1d77c6c875f8f95b015098e651c4c824ee
MrZece <domnulx7@gmail.com>
2025-03-01 13:55:59 +0200

    drawers and even more drawers
    the 3 drawers have an bug i mean feature

--- door-2.5d/multyopendrawers.gd
+++ door-2.5d/multyopendrawers.gd
@@ -4,19 +4,38 @@ extends StaticBody3D
 
 var plrishere = false
 var open1 = false
+var open2 = false
+var open3 = false
 
-func _ready() -> void:
-	pass # Replace with function body.
+var allopen = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("E") and plrishere:
-		if !open1:
-			ani.play("open1")
-			open1= true
-		else:
-			open1 = false
-			ani.play_backwards("open1")
+		if !allopen:
+			if !open3:
+				ani.play("open3")
+				open3= true
+			elif !open2:
+				ani.play("open2")
+				open2= true
+			elif !open1:
+				ani.play("open1")
+				open1= true
+			else:
+				allopen = true
+		if allopen:
+			if open3:
+				ani.play_backwards("open3")
+				open1= false
+			elif open2:
+				ani.play_backwards("open2")
+				open2= false
+			elif open1:
+				ani.play_backwards("open1")
+				open3= false
+			else:
+				allopen = false
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.is_in_group("plr"): #the plr is not in group plr yet and idk if i can change anything
