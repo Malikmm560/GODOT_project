a2678ecfe980b833305dfba3610c3ac00a911c89
MrZece <domnulx7@gmail.com>
2025-03-02 21:10:13 +0200

    UPD

--- door-2.5d/$cript/choosesfurniture.gd
+++ door-2.5d/$cript/choosesfurniture.gd
@@ -1,11 +1,11 @@
 extends Marker3D
 
-var furniture = ["res://$cene/basicdrawer.tscn",null,"res://$cene/largemultydrawer.tscn","res://$cene/longbasicdrawer.tscn","res://$cene/multydrawer.tscn",null]
+var furniture = ["res://$cene/basicdrawer.tscn","res://$cene/largemultydrawer.tscn","res://$cene/longbasicdrawer.tscn","res://$cene/multydrawer.tscn"]
 
 func _ready() -> void:
 	var rand = furniture[randi()%furniture.size()] #chooooooooses a random drawer/furniture
 	
-	if not(rand == null):
+	if randi_range(0,1)==1:
 		var obj = load(rand).instantiate()
 		call_deferred("add_sibling",obj)
 		obj.global_position = global_position
