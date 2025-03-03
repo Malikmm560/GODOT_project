8e2461fdcfa5e157cfbf5746e0ca407099a85df8
thorfinnking1 <157901908+thorfinnking1@users.noreply.github.com>
2025-03-03 21:42:36 +0200

    optimized inv

--- door-2.5d/$cript/item$.gd
+++ door-2.5d/$cript/item$.gd
@@ -14,104 +14,39 @@ func _on_body_entered(body):
 		$"../slot_1/Extraitems".frame = $Sprite3D.frame
 		$"../slot_1/Extraitems".visible = true
 		#if slot i$ empty it change$ the invi$ible $prite to the picked up item and make$ it vi$ible
-		if $Sprite3D.frame == 1:
-			$"../slot_1/Extraitems".visible = false
-			inv.coin += 5
-			inv.n -=1
-		elif $Sprite3D.frame == 2:
-			$"../slot_1/Extraitems".visible = false
-			inv.coin += 10
-			inv.n -=1
-		elif $Sprite3D.frame == 3:
-			$"../slot_1/Extraitems".visible = false
-			inv.coin += 20
-			inv.n -=1
-		#if frame 1,2 or 3 it add$ to the coin$ lable
 	elif inv.n == 2:
 		$"../slot_2/Extraitems2".frame = $Sprite3D.frame
 		$"../slot_2/Extraitems2".visible = true
 		#if slot i$ empty it change$ the invi$ible $prite to the picked up item and make$ it vi$ible
-		if $Sprite3D.frame == 1:
-			$"../slot_2/Extraitems2".visible = false
-			inv.coin += 5
-			inv.n -=1
-		elif $Sprite3D.frame == 2:
-			$"../slot_2/Extraitems2".visible = false
-			inv.coin += 10
-			inv.n -=1
-		elif $Sprite3D.frame == 3:
-			$"../slot_2/Extraitems2".visible = false
-			inv.coin += 20
-			inv.n -=1
-		#if frame 1,2 or 3 it add$ to the coin$ lable
 	elif inv.n == 3:
 		$"../slot_3/Extraitems3".frame = $Sprite3D.frame
 		$"../slot_3/Extraitems3".visible = true
-		#if slot i$ empty it change$ the invi$ible $prite to the picked up item and make$ it vi$ible
-		if $Sprite3D.frame == 1:
-			$"../slot_3/Extraitems3".visible = false
-			inv.coin += 5
-			inv.n -=1
-		elif $Sprite3D.frame == 2:
-			$"../slot_3/Extraitems3".visible = false
-			inv.coin += 10
-			inv.n -=1
-		elif $Sprite3D.frame == 3:
-			$"../slot_3/Extraitems3".visible = false
-			inv.coin += 20
-			inv.n -=1
-		#if frame 1,2 or 3 it add$ to the coin$ lable
 	elif inv.n == 4:
 		$"../slot_4/Extraitems4".frame = $Sprite3D.frame
 		$"../slot_4/Extraitems4".visible = true
 		#if slot i$ empty it change$ the invi$ible $prite to the picked up item and make$ it vi$ible
-		if $Sprite3D.frame == 1:
-			$"../slot_4/Extraitems4".visible = false
-			inv.coin += 5
-			inv.n -=1
-		elif $Sprite3D.frame == 2:
-			$"../slot_4/Extraitems4".visible = false
-			inv.coin += 10
-			inv.n -=1
-		elif $Sprite3D.frame == 3:
-			$"../slot_4/Extraitems4".visible = false
-			inv.coin += 20
-			inv.n -=1
-		#if frame 1,2 or 3 it add$ to the coin$ lable
+
 	elif inv.n == 5:
 		$"../slot_5/Extraitems5".frame = $Sprite3D.frame
 		$"../slot_5/Extraitems5".visible = true
 		#if slot i$ empty it change$ the invi$ible $prite to the picked up item and make$ it vi$ible
-		if $Sprite3D.frame == 1:
-			$"../slot_5/Extraitems5".visible = false
-			inv.coin += 5
-			inv.n -=1
-		elif $Sprite3D.frame == 2:
-			$"../slot_5/Extraitems5".visible = false
-			inv.coin += 10
-			inv.n -=1
-		elif $Sprite3D.frame == 3:
-			$"../slot_5/Extraitems5".visible = false
-			inv.coin += 20
-			inv.n -=1
-		#if frame 1,2 or 3 it add$ to the coin$ lable
 	elif inv.n == 6:
 		$"../slot_6/Extraitems6".frame = $Sprite3D.frame
 		$"../slot_6/Extraitems6".visible = true
 		#if slot i$ empty it change$ the invi$ible $prite to the picked up item and make$ it vi$ible
-		if $Sprite3D.frame == 1:
-			$"../slot_6/Extraitems6".visible = false
-			inv.coin += 5
-			inv.n -=1
-		elif $Sprite3D.frame == 2:
-			$"../slot_6/Extraitems6".visible = false
-			inv.coin += 10
-			inv.n -=1
-		elif $Sprite3D.frame == 3:
-			$"../slot_6/Extraitems6".visible = false
-			inv.coin += 20
-			inv.n -=1
-		#if frame 1,2 or 3 it add$ to the coin$ lable
 	if inv.n == 1 or inv.n == 2 or inv.n == 3 or inv.n == 4 or inv.n == 5 or inv.n == 6:
 		queue_free()
 		# if inv full it do$n't delete the item on the ground
+	if $Sprite3D.frame == 1:
+		$"../slot_6/Extraitems6".visible = false
+		inv.coin += 5
+		inv.n -=1
+	elif $Sprite3D.frame == 2:
+		$"../slot_6/Extraitems6".visible = false
+		inv.coin += 10
+		inv.n -=1
+	elif $Sprite3D.frame == 3:
+		$"../slot_6/Extraitems6".visible = false
+		inv.coin += 20
+		inv.n -=1
+		#if frame 1,2 or 3 it add$ to the coin$ lable
