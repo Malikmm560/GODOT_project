dde0d51780429a1c8e6c9009fc47113ff72efaf8
Malikmm560 <164187951+Malikmm560@users.noreply.github.com>
2025-03-03 16:45:16 +0100

    rotate flashlight latest (BUG)

--- door-2.5d/flashlight_v_2.gd
+++ door-2.5d/flashlight_v_2.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var flashlight_sprite = $Sprite3D
 @onready var flashlight_light = $SpotLight3D
-@onready var player = $"../.."
+@onready var player = $"../.."  
 
 var camera = null
 
@@ -15,13 +15,13 @@ func _process(delta):
 	if camera == null:
 		return
 
+	
 	var mouse_pos = get_viewport().get_mouse_position()
 	var ray_origin = camera.project_ray_origin(mouse_pos)
 	var ray_direction = camera.project_ray_normal(mouse_pos)
-	var target_pos = ray_origin + ray_direction * 20.0  # Distance of projection
+	var target_pos = ray_origin + ray_direction * 20.0
+	flashlight_light.position = target_pos
+	var direction = (Vector2(target_pos.x, target_pos.z) - Vector2(player.position.x, player.position.z)).normalized()
+	var angle = atan2(direction.y, direction.x)
 
-	# Make the spotlight aim at the target position
-	flashlight_light.look_at(target_pos, Vector3.UP)
-
-	# Make the flashlight itself rotate towards the target position
-	flashlight_sprite.look_at(target_pos, Vector3.UP)
+	rotation = Vector3(0, 0, angle)
